e0f424722e28ae90a54a2d4603dea6a8c057b260
Anila Alexander <anila.j.alexander@gmail.com>
2016-05-26 08:40:04 -0400

    second commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 ////////////////////////////////////////////////////BUBBLE CHART////////////////////////////////////////////////////
 //////////////////////////////////////Made using data about artist type, begin year and end year ///////////////////
 
@@ -135,3 +134,155 @@ d3.csv('data/artists.csv', function (error, data) {
       }
 }); // ends d3.csv for bubble chart
 
+////////////////////////////////////////////////////// DONUT CHART ////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var width = 550,
+    height = 350,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(radius - 70);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) { return d.ordering_attribute; });
+
+var svg = d3.select("#donutChart").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+d3.csv("data/series.csv", type, function(error, data) {
+  if (error) throw error;
+
+  var g = svg.selectAll(".arc")
+      .data(pie(data))
+      .enter().append("g")
+      .attr("class", "arc");
+
+  g.append("path")
+      .attr("d", arc)
+      .style("fill", function(d) { return color(d.data.type); });
+
+  g.append("text")
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .attr("dy", ".5em")
+      .text(function(d) { return d.data.type; });
+});
+
+function type(d) {
+  d.ordering_attribute = +d.ordering_attribute;
+  return d;
+}
+
+////////////////////////////////////////////////////// BAR GRAPHS ////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    var w = 550; 
+    var h = 390;
+
+    var colorBar = d3.scale.category20b();
+    var colorBarSecond = d3.scale.category20(); 
+
+    var dataArray = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ]; 
+ 
+    var xScale = d3.scale.ordinal()
+      .domain(d3.range(dataArray.length))
+      .rangeRoundBands([0, w], 0.05);
+
+    var yScale = d3.scale.linear()
+      .domain([0, d3.max(dataArray)])
+      .range([0,h]); 
+
+    var svgBar = d3.select("#bar")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h); 
+
+    svgBar.selectAll("rect")
+      .data(dataArray)
+      .enter()
+      .append("rect")  
+      .attr("x", function(d,i){
+        return xScale(i); 
+      })  
+      .attr("y", function(d) {
+        return h - yScale(d);
+      })
+      .attr("height", function(d) {
+        return yScale(d);
+      })
+      .attr("width", xScale.rangeBand())
+      .attr("height", function(d){
+        return yScale(d); 
+      })
+      .attr("fill", colorBar); 
+      // .attr("fill", function(d) {
+     //    return "rgb(0, 0, " + (d * 10) + ")";
+      // });
+
+    svgBar.selectAll("text")
+      .data(dataArray)
+      .enter()
+      .append("text")
+      .text(function(d) {
+        return d;
+      })
+      .attr("text-anchor", "middle")
+      .attr("x", function(d, i) {
+        return xScale(i) + xScale.rangeBand() / 2;
+      })
+      .attr("y", function(d) {
+        return h - yScale(d) + 14;
+      })
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "11px")
+      .attr("fill", "white");
+
+    //First Button
+
+    d3.select("#btn-one")
+      .on("click", function(){
+      
+    var dataArray = [ 11, 12, 15, 20, 18, 17, 16, 18, 23, 25, 
+                5, 10, 13, 19, 21, 25, 22, 18, 15, 13 ];
+
+        svgBar.selectAll("rect")
+          .data(dataArray)
+          .transition()
+          .duration(500)
+          .attr("y", function(d){
+            return h - yScale(d); 
+          })
+          .attr("height", function(d){
+            return yScale(d); 
+          })
+          // .attr("fill", function(d){
+          //  return "rgb(0, 0, " + (d * 10) + ")";
+          // }); 
+          .attr("fill", colorBarSecond); 
+
+      svgBar.selectAll("text")
+        .data(dataArray)
+        .transition()
+        .duration(500)
+        .text(function(d){
+          return d; 
+        })
+        .attr("x", function(d, i){
+          return xScale(i) + xScale.rangeBand() / 2; 
+        })
+        .attr("y", function(d){
+          return h - yScale(d) + 14; 
+        })
+
+      }); // ends on click
+
+    
